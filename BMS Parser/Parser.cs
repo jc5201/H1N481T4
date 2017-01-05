b798cb12670c688e9524bc358a9e18abd4ddb3e5
jc5201 <jch5201@naver.com>
2017-01-05 17:32:47 +0900

    CREATE BMS Parser

---
+++ BMS Parser/Parser.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace BMS_Parser
+{
+    class Parser
+    {
+        public string filename;
+        private Song song { get; }
+
+        public Song Parse()
+        {
+            StreamReader fs;
+            try
+            {
+                fs = new StreamReader(filename);
+            }
+            catch (FileNotFoundException)
+            {
+                return null;
+            }
+
+            while (true)
+            {
+                string line = fs.ReadLine();
+                if (line[0] != '#') continue;
+
+            }
+
+            return song;
+        }
+    }
+}
---
+++ BMS Parser/Song.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMS_Parser
+{
+    //참고 링크
+    //http://cosmic.mearie.org/2005/03/bmsguide/
+    //https://hitkey.nekokan.dyndns.info/cmds.htm#CONTROL-FLOW
+    class Song
+    {
+        public int PLAYER;
+        public string TITLE;
+        public string SUBTITLE;
+        public string ARTIST;
+        public string SUBARTIST;
+        public string MAKER;
+        public string GENRE;
+        public string COMMENT;  //""로 감싸인 문자열
+        public Single BPM;
+        public Single BASEBPM;
+        public int LEVEL;
+        public int DIFFICULTY;
+        //public int RANK;      //판정 조절
+        public int TOTALOBJECT;
+        public int VOLWAV;      //default 100
+        //public string MIDIBACK;
+        public string STAGEFILE;
+        public string BANNER;
+        public string VIDEOFILE;
+
+        List<string> WAVEFILE = new List<string>();
+
+        //xxxyy:aabbccdd
+        //      xxx:마디 번호
+        //      yy:채널 번호
+    }
+}
